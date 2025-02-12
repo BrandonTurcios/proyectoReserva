3c6ced3c98c0c89ee9b5e83e5b54675994d223b8
BRANDON <brandonturcios@unitec.edu>
2025-02-12 10:24:30 -0600

    hacer merge main

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
-// src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://figcohqizzoaptaqgsks.supabase.co'; // Replace with your Supabase URL
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpZ2NvaHFpenpvYXB0YXFnc2tzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY3Mzk5MjYsImV4cCI6MjA1MjMxNTkyNn0.TckWcvyJX-vA_F7R3WTczYMc9CQGhN40jwRPbDEmvP0'; // Replace with your Supabase Anon Key
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
